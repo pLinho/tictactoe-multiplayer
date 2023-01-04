16aad495c4358b943e2f7eb0b4740713f406fb02
Paulo Duarte Rettamozo <paulo.rettamozo@nsinova.onmicrosoft.com>
2023-01-04 17:53:16 -0300

    +++

--- NsinovaManager/Mapa.cs
+++ NsinovaManager/Mapa.cs
@@ -2,18 +2,35 @@
 {
     public class Mapa
     {
-        List<Player> players;
+        int widht = 800;
+        int height = 600;
 
-        void adicionarPlayer(string name) { 
+        List<Player> players = new List<Player>();
+
+        public List<Player> Players { get => players; set => players = value; }
+        public int Height { get => height; set => height = value; }
+        public int Widht { get => widht; set => widht = value; }
+
+        void adicionarPlayer(string name) {
+            Player novo_jogador = new Player();
+            novo_jogador.Name = name;
+            Random random = new Random();
+            novo_jogador.X = random.Next(widht);
+            novo_jogador.Y = random.Next(height);
+
+            Players.Add(novo_jogador);
         }
     }
 
     public class Player
     {
         int id;
+        int x;
+        int y;
         string name;
         public string Name { get => name; set => name = value; }
         public int Id { get => id; set => id = value; }
-
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
     }
 }
